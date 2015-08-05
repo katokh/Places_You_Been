7eac1456775740227297c8688e768debcb848a56
kt & sm <student@epicodus.com>
2015-08-05 15:35:20 -0700

    Places you've been updated to the app working properly

--- app/app.php
+++ app/app.php
@@ -1,13 +1,54 @@
 <?php
-  require_once __DIR__."/../vendor/autoload.php";
-  require_once __DIR__."/../src/Place.php";
+      require_once __DIR__."/../vendor/autoload.php";
+      require_once __DIR__."/../src/Place.php";
 
-  $app = new Silex\Application();
+      //SESSION - Stores Cookies
+      session_start();
 
-  //Twig Path
-  $app->register(new Silex\Provider\TwigServiceProvider(), array(
-      'twig.path' => __DIR__.'/../views'
-));
+      if(empty($_SESSION['list_of_cities'])) {
+        $_SESSION['list_of_cities'] = array();
+      }
 
-  return $app;
+      $app = new Silex\Application();
+
+      //Twig Path
+      $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+      ));
+
+      //Route and Controller
+      $app->get("/", function() use ($app) {
+
+        $all_cities = Place::getAll();
+
+        return $app['twig']->render('city.html.twig', array('cities' => $all_cities));
+
+      });
+
+    /*  $app->get("/", function() use ($app) {
+
+        $all_times = Place::getAll();
+
+        return $app['twig']->render('city.html.twig', array('times' => $all_times));
+
+      }); */
+
+      //cities POST
+      $app->post("/city", function() use ($app) {
+        $city = new Place($_POST['cityName'], $_POST['lengthOfTime']);
+        $city->save();
+
+        return $app['twig']->render('create_city.html.twig', array('newcity' => $city));
+      });
+
+
+      //cities POST delete
+      $app->post("/delete_city", function() use ($app) {
+
+        Place::deleteAll();
+
+        return $app['twig']->render('delete_city.html.twig');
+      });
+
+    return $app;
 ?>
--- src/Place.php
+++ src/Place.php
@@ -1,56 +1,56 @@
 <?php
-  class Place
-  {
-    private $cityName;
-    private $lengthOfTime;
+      class Place
+      {
+        private $cityName;
+        private $lengthOfTime;
 
-    //Constructor
-    function __construct($cityName, $lengthOfTime)
-    {
-      $this->city = $cityName;
-      $this->time = $lengthOfTime;
-    }
+        //Constructor
+        function __construct($cityName, $lengthOfTime)
+        {
+          $this->city = $cityName;
+          $this->time = $lengthOfTime;
+        }
 
-    //Setters
-    function setCityName($new_city_name)
-    {
-      $this->city = (string) $new_city_name;
-    }
+        //Setters
+        function setCityName($new_city_name)
+        {
+          $this->city = (string) $new_city_name;
+        }
 
-    function setLengthOfTime($new_length_of_time)
-    {
-      $this->time = (float) $new_length_of_time;
-    }
+        function setLengthOfTime($new_length_of_time)
+        {
+          $this->time = (float) $new_length_of_time;
+        }
 
-    //Getters
-    function getCityName()
-    {
-      return $this->city;
-    }
+        //Getters
+        function getCityName()
+        {
+          return $this->city;
+        }
 
-    function getLengthOfTime()
-    {
-      return $this->time;
-    }
+        function getLengthOfTime()
+        {
+          return $this->time;
+        }
 
-    //Save Method
-    function save()
-    {
-      array_push($_SESSION['list_of_cities'], $this);
-    }
+        //Save Method
+        function save()
+        {
+          array_push($_SESSION['list_of_cities'], $this);
+        }
 
-    //Getter Static Method
-    static function getAll()
-    {
-      return $_SESSION['list_of_cities'];
-    }
+        //Getter Static Method
+        static function getAll()
+        {
+          return $_SESSION['list_of_cities'];
+        }
 
-    //Static Method - Deletes Tasks
-    static function deleteAll()
-    {
-      $_SESSION['list_of_cities'] = array();
-    }
-  }
+        //Static Method - Deletes Tasks
+        static function deleteAll()
+        {
+          $_SESSION['list_of_cities'] = array();
+        }
+      }
 
 
 ?>
